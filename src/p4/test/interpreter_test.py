ac91da5e916bb6ed658a77e09f1ad54e695927a4
Dandan Zhao <dandanzhao08@gmail.com>
2025-04-29 13:45:50 +0200

    Fixing my branch

--- src/p4/test/interpreter_test.py
+++ src/p4/test/interpreter_test.py
@@ -1,15 +1,61 @@
 import unittest
 
-from interpreter import Interpreter
+from src.p4.interpreter import Interpreter
 
-from environment import Environment
+from src.p4.environment import Environment
 
-class TestInterpreter (unittest.testcase):
+
+class DummyNode:
+    def __init__(self, children):
+        self.children = children
+
+
+class test_add_sub(unittest.TestCase):
     def setUp(self):
-        self.env = Environment()
-        self.interpreter = Interpreter(self.env)
+        class Visitor:
+            def visit(self, node):
+                return node
 
-    def test_add_expr(self):
-        code = """ 
-        
-        """
+            new_intepreter = Interpreter()
+
+            def visit_add_expr(self, node):
+                result = self.visit(node.children[0])
+
+                for i in range(1, len(node.children), 2):
+                    op = node.children[i]
+                    value = self.visit(node.children[i + 1])
+
+                    if op == "+":
+                        result += value
+                    elif op == "-":
+                        result -= value
+                    else:
+                        raise Exception(f'Unsupported operator: {op}')
+                return result
+
+        self.visitor = Visitor()
+
+    def test_addition(self):
+        node = DummyNode([1, "+", 2, "+", 3])
+        result = self.visitor.visit_add_expr(node)
+        self.assertEqual(result, 6)
+
+    def test_addition_2(self):
+        node = DummyNode([-3, "+", 2, "+", 1])
+        result = self.visitor.visit_add_expr(node)
+        self.assertEqual(result, 0)
+
+    def test_subtraction(self):
+        node = DummyNode([3, "-", 2, "-", 1])
+        result = self.visitor.visit_add_expr(node)
+        self.assertEqual(result, 0)
+
+    def test_subtraction_2(self):
+        node = DummyNode([-1, "-", 2, "-", 3])
+        result = self.visitor.visit_add_expr(node)
+        self.assertEqual(result, -6)
+
+    def test_addition_and_subtraction(self):
+        node = DummyNode([10, "-", 3, "+", 2])
+        result = self.visitor.visit_add_expr(node)
+        self.assertEqual(result, 9)
