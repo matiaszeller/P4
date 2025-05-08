56b1ac9f628d046058abdc76ba085874c56b26b6
Matias Zeller <zellergaard@gmail.com>
2025-05-08 09:28:39 +0200

    fixes

--- src/p4/main.py
+++ src/p4/main.py
@@ -2,7 +2,7 @@ import sys
 from lark.tree import pydot__tree_to_png
 from semantics_checker import SemanticsChecker
 from src.p4.interpreter import Interpreter
-from src.p4.parser import extract_header, make_parser
+from src.p4.parse_tree_processor import extract_header, make_parser, ParseTreeProcessor
 
 def main():
     source_file = sys.argv[1] if len(sys.argv) > 1 else "sample.txt"
@@ -18,9 +18,12 @@ def main():
         lang, _ = extract_header(sample_input)
         parser = make_parser(lang)
         tree = parser.parse(sample_input)
+        processor = ParseTreeProcessor()
+        processed_tree = processor.transform(tree)
         print("Parse Tree:")
         print(tree.pretty())
         print(tree)
+        print(processed_tree)
         # Uncomment to update tree.png (need to install requirements.txt and graphviz from https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/12.2.1/windows_10_cmake_Release_graphviz-install-12.2.1-win64.exe)
         # pydot__tree_to_png(tree, "tree.png")
     except Exception as e:
